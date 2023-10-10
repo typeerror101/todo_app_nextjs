4e6cb794ac4d0b80423a76d4ed972b26d78cb7fd
Larosh Benny <laroshbenny@gmail.com>
2023-10-10 13:57:16 +0530

    day1

---
+++ src/app/new/page.tsx
@@ -0,0 +1,7 @@
+export default function neww() {
+  return (
+    <div>
+      <h1> neww</h1>
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+   <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link 
+          href="/new"
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+          hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        >New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        
+      </ul>
+   </>
+  )
+}
