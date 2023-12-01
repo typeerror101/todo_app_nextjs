62bb8076b52e97f20e3e2406a2bebf543356a59c
Larosh Benny <laroshbenny@gmail.com>
2023-12-01 23:09:29 +0530

    upadte

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,4 +1,19 @@
+import { error } from "console";
 import Link from "next/link";
+import { prisma } from "../db";
+import { redirect } from "next/navigation";
+
+async function createTodo(data: FormData){
+  "use server"
+
+  const title = data.get("title")?.valueOf()
+  if(typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid Title")
+  }
+
+  await prisma.todo.create({ data: {title, complete: false } })
+  redirect("/")
+}
 
 export default function neww() {
   return (
@@ -7,7 +22,7 @@ export default function neww() {
         <h1 className="text-2xl">New</h1>
         
       </header>
-      <form className="flex gap-2 flex-col">
+      <form  action={createTodo} className="flex gap-2 flex-col">
         <input
             type="text"
             name="title"
